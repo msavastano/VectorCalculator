fa37dde45c5b5847ff3d7ee7f52b0db1a2571e75
msavastano <millhouse5000@gmail.com>
2013-11-19 19:06:47 -0500

    Create RADIANSDEGREES.py

---
+++ RADIANSDEGREES.py
@@ -0,0 +1,11 @@
+import math
+#import VECTORS
+
+def rad2degrees(r):
+    return (180/math.pi) * r
+
+def degrees2rad(d):
+    return d * (math.pi/180)
+
+def pyth(x,y):
+    return math.sqrt(x**2 + y**2)
